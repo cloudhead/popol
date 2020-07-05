c3d2e81e1d9bb4e1572a55319bac46b03d36834d
Alexis Sellier <self@cloudhead.io>
2020-07-05 08:39:16 +0200

    [example] Make `tcp` example more realistic

--- examples/tcp.rs
+++ examples/tcp.rs
@@ -9,6 +9,7 @@ fn main() -> io::Result<()> {
     let mut stream = net::TcpStream::connect("localhost:8888").unwrap();
     let mut descriptors = Descriptors::new();
 
+    stream.set_nonblocking(true)?;
     descriptors.register(stream.peer_addr()?, &stream, popol::events::READ);
 
     loop {
@@ -18,13 +19,25 @@ fn main() -> io::Result<()> {
             if event.readable {
                 let mut buf = [0; 32];
 
-                let n = stream.read(&mut buf[..])?;
-                if n == 0 {
-                    return stream.shutdown(net::Shutdown::Both);
+                loop {
+                    match stream.read(&mut buf[..]) {
+                        Ok(n) if n > 0 => {
+                            let msg = std::str::from_utf8(&buf[..n]).unwrap();
+                            println!("{}: {}", addr, msg.trim());
+                        }
+                        Ok(_) => {
+                            // Connection closed.
+                            return stream.shutdown(net::Shutdown::Both);
+                        }
+                        Err(err) if err.kind() == io::ErrorKind::WouldBlock => {
+                            // Nothing left to read.
+                            break;
+                        }
+                        Err(err) => {
+                            panic!(err);
+                        }
+                    }
                 }
-
-                let msg = std::str::from_utf8(&buf[..n]).unwrap();
-                print!("{}: {}", addr, msg);
             }
         }
     }
